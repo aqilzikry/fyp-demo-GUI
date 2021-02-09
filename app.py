bd4eca1b170de0aa00a956f68996cbc919e93709
Aqilzikry <aqilzikry97@gmail.com>
2021-02-10 00:55:11 +0800

    get filenames of uploaded files

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import os
-from flask import Flask, request, render_template
+import glob
+import time
+from flask import Flask, request, render_template, redirect, url_for
 from flask_dropzone import Dropzone
 
 app = Flask(__name__)
 
 dropzone = Dropzone(app)
+
+app_path = os.path.dirname(os.path.realpath(__file__))
+files = {}
 
 @app.route('/uploads', methods=['GET', 'POST'])
 def upload():
@@ -12,7 +17,11 @@ def upload():
         f = request.files.get('file')
         f.save(os.path.join('files', f.filename))
 
-    return render_template('index.html')
+    files.clear()
+    for file in os.listdir(os.path.join(app_path, 'files')):
+        files[file] = time.ctime(os.path.getctime(os.path.join(app_path, 'files', file)))
+
+    return render_template('index.html', files = files)
 
 if __name__ == '__main__':
     app.run(debug=True)
