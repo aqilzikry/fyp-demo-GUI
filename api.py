2c912fcbd2dd4163d4eb7acbf394ad5f69866cf8
Aqilzikry <aqilzikry97@gmail.com>
2021-02-11 02:28:42 +0800

    return json as response

--- api.py
+++ api.py
@@ -1,14 +1,14 @@
 import flask
 import engine
 import json
+from flask import jsonify, make_response
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    result = dict(engine.main())
-    print(type(result))
-    return result
+    result = engine.main()
+    return make_response(jsonify(result), 200)
 
-app.run()
+app.run(host='127.0.0.1', port=7000)
--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
 import glob
 import time
-from flask import Flask, request, render_template, redirect, url_for
+import requests
+import json
+import urllib.request
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from flask_dropzone import Dropzone
 
 app = Flask(__name__)
 
 dropzone = Dropzone(app)
+
+URL = "http://127.0.0.1:7000"
 
 app_path = os.path.dirname(os.path.realpath(__file__))
 files = {}
@@ -21,7 +26,14 @@ def upload():
     for file in os.listdir(os.path.join(app_path, 'files')):
         files[file] = time.ctime(os.path.getctime(os.path.join(app_path, 'files', file)))
 
-    return render_template('index.html', files = files)
+    #with urllib.request.urlopen(URL) as url:
+    #    data = json.loads(url.read().decode())
+
+    respond = requests.get(URL)
+    json_data = respond.json()
+    json_data = json.dumps(json_data, indent=4)
+
+    return render_template('index.html', files = files, data = json_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
